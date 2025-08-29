2b3cc486d8721ca0391bbfaecd9afe001839bf35
brendan-e-mckinley <brendan_mckinley@mines.edu>
2025-08-29 13:57:51 -0600

    add fiber object

---
+++ new code/fiber.py
@@ -0,0 +1,7 @@
+class Fiber:
+    def __init__(self, tau):
+        self.tau = tau
+
+    def get_bishop_frame():
+        # To Do: infer bishop frame from vector soup
+        print('Get bishop frame')
